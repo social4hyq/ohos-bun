af57fb10d03772dea9916ebe4973a428d582d147
social4hyq <social4hyq@noreply.gitcode.com>
2026-07-09 07:31:57 +0800

    fix: remove unused AbsPath import

--- src/install/hoisted_install.rs
+++ src/install/hoisted_install.rs
@@ -5,7 +5,7 @@ use core::sync::atomic::Ordering;
 use bun_collections::{DynamicBitSet as Bitset, DynamicBitSetList, StringHashMap};
 use bun_core::strings;
 use bun_core::{Global, Output};
-use bun_paths::{AbsPath, SEP};
+use bun_paths::SEP;
 use bun_sys::{self as sys, Dir, Fd};
 
 use crate::analytics;
